346df2e9caf8abe7e5c799725fa84de4fee0ccbe
刘毅豪 <liuyihao@krspace.cn>
2017-07-10 19:04:49 +0800

    fix

--- src/Utils/PublicFn/index.js
+++ src/Utils/PublicFn/index.js
@@ -1,5 +1,10 @@
 var arr1 = ['1', '2', '3', '4', '5', '6', '6', '7', '8', '1', '1', '1'];
 console.log(allRepeat(arr1))
+/**
+ * allRepeat 循环数组并返回所有重复值的下标
+ * @arr  {arr}  需要检测的数组
+ * @return {arr} 所有重复下表的集合
+ */
 function allRepeat(arr) {
 	var yesList = [];
 
@@ -22,3 +27,34 @@ function allRepeat(arr) {
 	return yesList;
 }
 
+
+
+
+
+/**
+ * computeStyle 获取节点的css属性值
+ * @elem  {node}     html节点
+ * @prop  {string}   css属性
+ * @return {string}  css属性值
+ */
+function computeStyle(elem, prop) {
+
+		 if (!window.getComputedStyle) {
+				 window.getComputedStyle = function(el, pseudo) {
+						 this.el = el;
+						 this.getPropertyValue = function(prop) {
+								 var re = /(\-([a-z]){1})/g;
+								 if (prop == 'float') prop = 'styleFloat';
+								 if (re.test(prop)) {
+										 prop = prop.replace(re, function () {
+												 return arguments[2].toUpperCase();
+										 });
+								 }
+								 return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
+						 }
+						 return this;
+				 }
+		 }
+
+		 return window.getComputedStyle(elem, null).getPropertyValue(prop);
+ }
